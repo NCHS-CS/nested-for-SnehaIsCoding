50271bc1a6f50f0168467da300ddf7f76be2b414
SnehaIsCoding <1116572@apps.nsd.org>
2024-10-07 04:46:14 +0000

    Done

--- src/main/java/org/nhs/app/App.java
+++ src/main/java/org/nhs/app/App.java
@@ -1,8 +1,42 @@
-/*----------------------------------------------------------------------------------------
- * Copyright (c) Microsoft Corporation. All rights reserved.
- * Licensed under the MIT License. See LICENSE in the project root for license information.
- *---------------------------------------------------------------------------------------*/
+public class App{
+    public static void main(String[] args){
+        warmup1();
+        System.out.println();
+        warmup2();
+        System.out.println();
+        warmup3();
+    }
+        public static void warmup1(){
+        for (int number = 1; number <= 5; number ++){
+            for (int counter = number; counter >= 1; counter --){
+               System.out.print(number);
+            } 
+          System.out.println();
+        }
+    }
+    public static void warmup2(){
+        for (int numb = 1; numb <= 5; numb ++){
+            for (int numdot = 1; numdot <= 5 - numb; numdot ++){
+                System.out.print(".");
+            }
+            for (int numdigits = 1; numdigits <= numb; numdigits ++){
+                System.out.print(numb);
+            }
+            System.out.println();
+        }
+    }
+    public static void warmup3(){
+        for (int numb = 1; numb <= 5; numb ++){
+            for (int numdot = 1; numdot <= 5; numdot ++){
+                if(numdot == 6 - numb) {
+                    System.out.print(numb);
+                }
+                else {
+                    System.out.print(".");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
 
-package org.nhs.app;
-
-// Your code goes here
